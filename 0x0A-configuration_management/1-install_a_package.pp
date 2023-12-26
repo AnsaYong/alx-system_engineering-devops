745126371f4b018896c718f6cf69017a1b8d136e
AnsaYong <ansahmbomyong@gmail.com>
2023-12-26 17:08:34 +0200

    updated 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -11,5 +11,5 @@ exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',   # Specify the desired version
   path    => ['/usr/bin'],                           # Set the pah to include pip3
   unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0', # Check if Flask is already installed with the desired version
-  require => Package['python3-pip'],                 # Ensure that pip3 is installed before running the exec
+#require => Package['python3-pip'],                 # Ensure that pip3 is installed before running the exec
 }
