57f380ac13228c5b1bae0348ee0f0a3364daba07
AnsaYong <ansahmbomyong@gmail.com>
2023-12-29 13:12:22 +0200

    updated 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,32 +1,32 @@
 # A manifest that sets up the client SSH configuration file so that
 # you can connect to a server without typing a password
 #
-# This class uses the `ssh_authorized_key` approach rather than the
-# `file_line` approach
+# This class uses the `file_line` approach rather than the
+# `ssh_authorized_key` approach
 class ssh_config {
   # Define class for managing SSH configuration
+  # Set up SSH key authentication using the file_line approach
 
-  # Set up SSH key authentication
-  ssh_authorized_key { 'school':
-    ensure => present,
-    user   => 'ubuntu',             # Username
-    type   => 'ssh-rsa',            # type of key
-    key    => '~/.ssh/school.pub',  # path to the public key
+  # Turn off password authentication
+  file_line { 'Turn off password authentication':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',  # replaces the line in the file that matches the pattern below
+    match  => '^#?\s*PasswordAuthentication\s+.*$', # pattern to find in the file
   }
 
-  # Disable password authentication
-  file { '/etc/ssh/sshd_config':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0744',
-    content => template('ssh_config/ssh_config.erb'),
-    notify  => Service['ssh'],
+  # specify the identify file - contains the RSA private key
+  file_line { 'Declare identity file':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school',
+    match  => '^#?\s*IdentityFile\s+.*$',
   }
 
+  # Reload the SSH service when the configuration changes
   service { 'ssh':
     ensure  => running,
     enable  => true,
-    require => File['/etc/ssh/sshd_config'],
+    require => [File_line['Turn off password authentication'], File_line['Declare identity file']],
   }
 }
