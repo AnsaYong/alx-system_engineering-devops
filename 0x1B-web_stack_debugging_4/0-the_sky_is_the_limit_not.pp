bf872c9b3b951ccdfd8e222ea7bea6e17fa3c942
Ansahmbom <ansahmbomyong@gmail.com>
2024-02-21 08:04:06 +0200

    added 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Update the maximum limit for open files
+exec {'increase max open files limit':
+  command => 'sed -i "s|15|15000|g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+}
+
+exec {'restart nginx':
+  require => Exec['increase max open files limit'],
+  command => 'sudo service nginx restart',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+}
